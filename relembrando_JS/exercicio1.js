b0d15fd826b2e4a94f79294a081a32d20262d9de
Pedro Cella <pedrovscella@gmail.com>
2021-12-21 10:52:01 -0300

    Relembrando JS

---
+++ relembrando_JS/exercicio1.js
@@ -0,0 +1,10 @@
+let endereco = {
+  rua: "Rua da paz",
+  numero: 42,
+  bairro: "Cachoeira",
+  cidade: "Campo Grande",
+  uf: "MS"
+};
+
+console.log(`O usuario mora em ${endereco.cidade}/${endereco.uf}, no bairro ${endereco.bairro}, na ${endereco.rua},
+numero ${endereco.numero}`)
